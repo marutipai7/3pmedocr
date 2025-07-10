9efd8f3181f7733d327637c700434484c676904d
Mona <monig5658@gmail.comm>
2025-07-10 09:14:34 +0530

    added env file

--- core/settings.py
+++ core/settings.py
@@ -1,16 +1,14 @@
 import os
-import gridfs
 from pathlib import Path
-from pymongo import MongoClient
-from mongoengine import connect
+from dotenv import load_dotenv
 
+# Load .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = 'django-insecure-zpf%ym5!640&3vgl5npe@2t=trxnsjrg66h(9-!ysyj4i5jp*5'
-
-DEBUG = True
-
-ALLOWED_HOSTS = ["192.168.1.99","mediocr.aibuzz.net"]
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h.strip()]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -64,66 +62,48 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medocrcrm',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',  # or your server
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Kolkata'
-
 USE_I18N = True
-
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/document/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'document')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 LOGIN_URL = '/'
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
+    'handlers': {'console': {'class': 'logging.StreamHandler'}},
+    'root': {'handlers': ['console'], 'level': 'INFO'},
 }
 
-MONGO_DATABASE_NAME = 'Database_1'
-MONGO_DATABASE_HOST = "mongodb://localhost:27017/"
+# MongoDB (mongoengine and pymongo)
+from mongoengine import connect
+from pymongo import MongoClient
+
+MONGO_DATABASE_NAME = os.getenv('MONGO_DATABASE_NAME')
+MONGO_DATABASE_HOST = os.getenv('MONGO_DATABASE_HOST')
 
 connect(
     db=MONGO_DATABASE_NAME,
@@ -133,7 +113,6 @@ connect(
 MONGO_CLIENT = MongoClient(MONGO_DATABASE_HOST)
 MONGO_DB = MONGO_CLIENT[MONGO_DATABASE_NAME]
 
-# Collections
 MONGO_COLLECTIONS = {
     "hospital": MONGO_DB["Hospitals"],
     "pharmacy": MONGO_DB["Pharmacy"],
@@ -142,13 +121,11 @@ MONGO_COLLECTIONS = {
 }
 PLACES_COORDINATES = MONGO_DB["places"]
 
-# AUTH_USER_MODEL = 'registration.User'
-#mail setting 
+# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yourprovider.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your@email.com'
-EMAIL_HOST_PASSWORD = 'your_password'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Your App <your@email.com>'
-
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
