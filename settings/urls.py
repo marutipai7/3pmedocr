5626abc6433cff123b4f3c84749a7905b6125264
MARUTI PAI <marutipai203@gmail.com>
2025-12-16 18:18:53 +0530

    fixes

--- settings/urls.py
+++ settings/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    ## settings
     path('', views.settings_page, name='settings_page'),
     path('update-ngo-profile/', views.update_ngo_profile, name='update_ngo_profile'),
     path('update-advertiser-profile/', views.update_advertiser_profile, name='update_advertiser_profile'),
@@ -18,4 +19,36 @@ urlpatterns = [
     path('change-password/', views.change_password, name='change_password'),
     # path('get_user_theme_api/', views.get_user_theme_api, name='get_user_theme_api'),
     # path('account-details/', views.get_account_details, name='account-details'),
+
+    ## donate
+    path('donate/', views.donate_view, name='donate'),
+    path('donate-pay/<int:post_id>/', views.donate_pay_view, name='donate-pay'),
+    path('donation-history/', views.get_donation_history, name='donation-history-ajax'),
+    path('export-donation-history/', views.export_donation_history, name='export-donation-history'),
+    path('get-donate-bill/<int:donation_id>/', views.get_donate_bill, name='get-donate-bill'),
+    path('get-platform-bill/<int:donation_id>/', views.get_platform_bill, name='get-platform-bill'),
+    path('toggle-saved/', views.toggle_saved_donation, name='toggle_saved_donation'),
+    path('get-organization-posts/', views.get_organization_posts, name='get-organization-posts'),
+
+    ## rewards
+    path('rewards/', views.points_dashboard, name='points'),
+    path('get-cards/', views.get_coupon_cards, name='get_cards'),
+    path('get-popular-coupons/', views.get_popular_coupon_cards, name='get_popular_coupon_cards'),
+    path('history/', views.points_history_view, name='points_history'),
+    path('history/ajax/', views.ajax_filtered_points, name='ajax_filtered_history'),
+    path('claim-coupon/', views.claim_coupon, name='claim-coupon'),
+    path("claimed-coupons/ajax/", views.get_claimed_coupons, name="get_claimed_coupons"),
+
+    ## support
+    path('support/', views.support_view, name='support_view'),
+    path('submit-ticket/', views.submit_support_ticket, name='submit_support_ticket'),
+    path('filter-tickets/', views.filter_support_tickets, name='filter_support_tickets'),
+    path('get-bot-content/', views.get_bot_content_api, name='get_bot_content_api'),
+    path('get-user-tickets/', views.get_user_tickets, name='get_user_tickets'),
+    path('get-issue-options/', views.get_issue_options, name='get-issue-options'),
+    path('get-tickets-list/', views.get_ticket_lists, name='get-tickets-list'),
+    path('get-tickets-details/', views.ticket_details, name='get-tickets-details'),
+    path('get-tickets-filters/', views.filter_tickets, name='get-tickets-filters'),
+    path('get-faq-lists/', views.faq_lists, name='get-faq-lists'),
+    path('send-support-email/', views.send_support_email, name='send-support-email'),
 ]
