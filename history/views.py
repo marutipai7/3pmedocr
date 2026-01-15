3f51c964ee6729320f549a3afa32b728fa21484f
MARUTI PAI <marutipai203@gmail.com>
2026-01-15 14:05:13 +0530

    fixed services and added pharmacy history

--- history/views.py
+++ history/views.py
@@ -7,4 +7,6 @@ from dashboard.utils import dashboard_login_required, get_common_context
 def history(request):
     user = request.user_obj
     context = get_common_context(request,user)
+    if user.user_type == 'pharmacy':
+        return render(request, 'pharmacy/history.html', context)
     return render(request, 'history.html', context)
